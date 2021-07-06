56570012c863fe0ca7b12bb11ac2fa5f92a0909f
frazierpleb <andrew.mantilla@obf.ateneo.edu>
2021-07-06 21:31:46 +0800

    Commit Test

--- Introduction.R
+++ Introduction.R
@@ -1,2 +1,5 @@
-print("Hi! My Name is Andrew Mantilla")
-print("I'm 19 years old. I'm currently taking up the ECE Course in Ateneo De Manila University.")
+x <- "Hi! My Name is Andrew Mantilla"
+y <- "I'm 19 years old. I'm currently taking up the ECE Course in Ateneo De Manila University."
+print(x)
+print(y)
+
