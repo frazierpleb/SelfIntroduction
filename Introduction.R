3b55f9f7287a364ddfe8bbfa04b4a7c0848f49bd
frazierpleb <andrew.mantilla@obf.ateneo.edu>
2021-07-06 21:40:06 +0800

    Final Commit for intro

--- Introduction.R
+++ Introduction.R
@@ -1,5 +1,12 @@
-x <- "Hi! My Name is Andrew Mantilla"
-y <- "I'm 19 years old. I'm currently taking up the ECE Course in Ateneo De Manila University."
+ w<- "Hi! My Name is Andrew Mantilla"
+ x<- "I'm 19 years old and I'm currently taking up the ECE Course in Ateneo De Manila University."
+ y<- "My hobbies are playing computer games and musical instruments"
+ z<- "One thing that I'm trying to learn this break is blender. Blender can be used for 2d or 3d rendering."
+print(w)
+
 print(x)
+
 print(y)
+
+print(z)
 
